cee6275d7425844bd433a9f96981ae6fce4e3549
dalma11 <sosimin82@gmail.com>
2019-08-19 20:11:49 +0900

    AJP 포트 추가 / 배포환경 설정 추가

--- src/main/java/com/cfl/config/WebConfig.java
+++ src/main/java/com/cfl/config/WebConfig.java
@@ -1,7 +1,12 @@
 package com.cfl.config;
 
 import com.cfl.interceptor.NetworkAclInterceptor;
+
+import org.apache.catalina.connector.Connector;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -14,6 +19,12 @@ import java.util.List;
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Autowired
     private NetworkAclInterceptor networkAclInterceptor;
+    
+    @Value("${tomcat.ajp.protocol}")
+    String tomcatAjpProtocal;
+    
+    @Value("${tomcat.ajp.port}")
+    String tomcatAjpPort;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry){
@@ -31,4 +42,23 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public WildcardPathVariableHandlerMethodArgumentResolver wildcardPathVariableHandlerMethodArgumentResolver() {
         return new WildcardPathVariableHandlerMethodArgumentResolver();
     }
+    
+    
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createAjpConnector());
+		return tomcat;
+	}
+
+	
+	
+	private Connector createAjpConnector() {
+		Connector ajpConnector = new Connector(tomcatAjpProtocal);
+		ajpConnector.setPort(Integer.parseInt(tomcatAjpPort));
+		ajpConnector.setSecure(false);
+		ajpConnector.setAllowTrace(false);
+		ajpConnector.setScheme("http");
+		return ajpConnector;
+	}
 }
